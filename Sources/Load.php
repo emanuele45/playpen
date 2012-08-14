52da1b9fa94f952c618740e97aae089be73062b5
emanuele <emanuele45@gmail.com>
2012-08-14 18:05:41 +0200

    Splitting up cache in multiple function should be easier to expand
    Signed-off-by: emanuele <emanuele45@gmail.com>

--- Sources/Load.php
+++ Sources/Load.php
@@ -2645,102 +2645,138 @@ function cache_put_data($key, $value, $ttl = 120)
 		$st = microtime();
 	}
 
-	$key = md5($boardurl . filemtime($sourcedir . '/Load.php')) . '-SMF-' . strtr($key, ':/', '-_');
+	$key = md5($boardurl . filemtime($sourcedir . '/Load.php')) . '-SMF-' . $key;
 	$value = $value === null ? null : serialize($value);
 
-	switch ($cache_accelerator)
-	{
-		case 'memcached':
-			// The simple yet efficient memcached.
-			if (function_exists('memcached_set') || function_exists('memcache_set') && isset($modSettings['cache_memcached']) && trim($modSettings['cache_memcached']) != '')
-			{
-				// Not connected yet?
-				if (empty($memcached))
-					get_memcached_server();
-				if (!$memcached)
-					return;
-
-				memcache_set($memcached, $key, $value, 0, $ttl);
-			}
-			break;
-		case 'eaccelerator':
-			// eAccelerator...
-			if (function_exists('eaccelerator_put'))
-			{
-				if (mt_rand(0, 10) == 1)
-					eaccelerator_gc();
-
-				if ($value === null)
-					@eaccelerator_rm($key);
-				else
-					eaccelerator_put($key, $value, $ttl);
-			}
-			break;
-		case 'mmcache':
-			// Turck MMCache?
-			if (function_exists('mmcache_put'))
-			{
-				if (mt_rand(0, 10) == 1)
-					mmcache_gc();
-
-				if ($value === null)
-					@mmcache_rm($key);
-				else
-				{
-					mmcache_lock($key);
-					mmcache_put($key, $value, $ttl);
-					mmcache_unlock($key);
-				}
-			}
-			break;
-		case 'apc':
-			// Alternative PHP Cache, ahoy!
-			if (function_exists('apc_store'))
-			{
-				// An extended key is needed to counteract a bug in APC.
-				if ($value === null)
-					apc_delete($key . 'smf');
-				else
-					apc_store($key . 'smf', $value, $ttl);
-			}
-			break;
-		case 'zend':
-			// Zend Platform/ZPS/etc.
-			if (function_exists('zend_shm_cache_store'))
-				zend_shm_cache_store('SMF::' . $key, $value, $ttl);
-			elseif (function_exists('output_cache_put'))
-				output_cache_put($key, $value);
-			break;
-		case 'xcache':
-			if (function_exists('xcache_set') && ini_get('xcache.var_size') > 0)
-			{
-				if ($value === null)
-					xcache_unset($key);
-				else
-					xcache_set($key, $value, $ttl);
-			}
-			break;
-		default:
-			// Otherwise custom cache?
-			if ($value === null)
-				@unlink($cachedir . '/data_' . $key . '.php');
-			else
-			{
-				$cache_data = '<' . '?' . 'php if (!defined(\'SMF\')) die; if (' . (time() + $ttl) . ' < time()) $expired = true; else{$expired = false; $value = \'' . addcslashes($value, '\\\'') . '\';}' . '?' . '>';
-
-				// Write out the cache file, check that the cache write was successful; all the data must be written
-				// If it fails due to low diskspace, or other, remove the cache file
-				if (file_put_contents($cachedir . '/data_' . $key . '.php', $cache_data, LOCK_EX) !== strlen($cache_data))
-					@unlink($cachedir . '/data_' . $key . '.php');
-			}
-			break;
-	}
-
-	if (function_exists('call_integration_hook'))
-		call_integration_hook('cache_put_data', array(&$key, &$value, &$ttl));
+	$function = 'smf_put_' . $cache_accelerator;
+	if ($function($key, $value, $ttl) === false)
+		smf_put_filecache($key, $value, $ttl);
 
 	if (isset($db_show_debug) && $db_show_debug === true)
 		$cache_hits[$cache_count]['t'] = array_sum(explode(' ', microtime())) - array_sum(explode(' ', $st));
+}
+
+function smf_put_memcached($key, $value, $ttl)
+{
+	// The simple yet efficient memcached.
+	if (function_exists('memcached_set') || function_exists('memcache_set') && isset($modSettings['cache_memcached']) && trim($modSettings['cache_memcached']) != '')
+	{
+		// Not connected yet?
+		if (empty($memcached))
+			get_memcached_server();
+		if (!$memcached)
+			return;
+
+		memcache_set($memcached, $key, $value, 0, $ttl);
+		return true;
+	}
+
+	return false;
+}
+
+function smf_put_eaccelerator($key, $value, $ttl)
+{
+	// eAccelerator...
+	if (function_exists('eaccelerator_put'))
+	{
+		if (mt_rand(0, 10) == 1)
+			eaccelerator_gc();
+
+		if ($value === null)
+			@eaccelerator_rm($key);
+		else
+			eaccelerator_put($key, $value, $ttl);
+
+		return true;
+	}
+
+	return false;
+}
+
+function smf_put_mmcache($key, $value, $ttl)
+{
+	// Turck MMCache?
+	if (function_exists('mmcache_put'))
+	{
+		if (mt_rand(0, 10) == 1)
+			mmcache_gc();
+
+		if ($value === null)
+			@mmcache_rm($key);
+		else
+		{
+			mmcache_lock($key);
+			mmcache_put($key, $value, $ttl);
+			mmcache_unlock($key);
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+function smf_put_apc($key, $value, $ttl)
+{
+	// Alternative PHP Cache, ahoy!
+	if (function_exists('apc_store'))
+	{
+		// An extended key is needed to counteract a bug in APC.
+		if ($value === null)
+			apc_delete($key . 'smf');
+		else
+			apc_store($key . 'smf', $value, $ttl);
+
+		return true;
+	}
+
+	return false;
+}
+
+function smf_put_zend($key, $value, $ttl)
+{
+	// Zend Platform/ZPS/etc.
+	if (function_exists('zend_shm_cache_store'))
+		zend_shm_cache_store('SMF::' . $key, $value, $ttl);
+	elseif (function_exists('output_cache_put'))
+		output_cache_put($key, $value);
+	else
+		return false;
+
+	return true;
+}
+
+function smf_put_xcache($key, $value, $ttl)
+{
+	if (function_exists('xcache_set') && ini_get('xcache.var_size') > 0)
+	{
+		if ($value === null)
+			xcache_unset($key);
+		else
+			xcache_set($key, $value, $ttl);
+
+		return true;
+	}
+
+	return false;
+}
+
+function smf_put_filecache($key, $value, $ttl)
+{
+	// Otherwise custom cache?
+	$key = strtr($key, ':/', '-_');
+
+	if ($value === null)
+		@unlink($cachedir . '/data_' . $key . '.php');
+	else
+	{
+		$cache_data = '<' . '?' . 'php if (!defined(\'SMF\')) die; if (' . (time() + $ttl) . ' < time()) $expired = true; else{$expired = false; $value = \'' . addcslashes($value, '\\\'') . '\';}' . '?' . '>';
+
+		// Write out the cache file, check that the cache write was successful; all the data must be written
+		// If it fails due to low diskspace, or other, remove the cache file
+		if (file_put_contents($cachedir . '/data_' . $key . '.php', $cache_data, LOCK_EX) !== strlen($cache_data))
+			@unlink($cachedir . '/data_' . $key . '.php');
+	}
 }
 
 /**
@@ -2768,11 +2804,23 @@ function cache_get_data($key, $ttl = 120)
 		$st = microtime();
 	}
 
-	$key = md5($boardurl . filemtime($sourcedir . '/Load.php')) . '-SMF-' . strtr($key, ':/', '-_');
+	$key = md5($boardurl . filemtime($sourcedir . '/Load.php')) . '-SMF-' . $key;
 
-	switch ($cache_accelerator)
+	$function = 'smf_get_' . $cache_accelerator;
+	$value = $function($key, $ttl);
+	if ($function($key, $ttl) === null)
+		$value = smf_get_filecache($key, $ttl);
+
+	if (isset($db_show_debug) && $db_show_debug === true)
 	{
-		case 'memcache':
+		$cache_hits[$cache_count]['t'] = array_sum(explode(' ', microtime())) - array_sum(explode(' ', $st));
+		$cache_hits[$cache_count]['s'] = isset($value) ? strlen($value) : 0;
+	}
+
+	return empty($value) ? null : @unserialize($value);
+
+function smf_get_memcache($key, $ttl)
+{
 			// Okay, let's go for it memcached!
 			if ((function_exists('memcache_get') || function_exists('memcached_get')) && isset($modSettings['cache_memcached']) && trim($modSettings['cache_memcached']) != '')
 			{
@@ -2785,57 +2833,70 @@ function cache_get_data($key, $ttl = 120)
 				$value = (function_exists('memcache_get')) ? memcache_get($cache['connection'], $key) : memcached_get($cache['connection'], $key);
 			}
 			break;
-		case 'eaccelerator':
-			// Again, eAccelerator.
-			if (function_exists('eaccelerator_get'))
-				$value = eaccelerator_get($key);
-			break;
-		case 'mmcache':
-			// The older, but ever-stable, Turck MMCache...
-			if (function_exists('mmcache_get'))
-				$value = mmcache_get($key);
-			break;
-		case 'apc':
-			// This is the free APC from PECL.
-			if (function_exists('apc_fetch'))
-				$value = apc_fetch($key . 'smf');
-			break;
-		case 'zend':
-			// Zend's pricey stuff.
-			if (function_exists('zend_shm_cache_fetch'))
-				$value = zend_shm_cache_fetch('SMF::' . $key, $ttl);
-			elseif (function_exists('output_cache_get'))
-				$value = output_cache_get($key, $ttl);
-			break;
-		case 'xcache':
-			if (function_exists('xcache_get') && ini_get('xcache.var_size') > 0)
-				$value = xcache_get($key);
-			break;
-		default:
-			// Otherwise it's SMF data!
-			if (file_exists($cachedir . '/data_' . $key . '.php') && filesize($cachedir . '/data_' . $key . '.php') > 10)
-			{
-				// php will cache file_exists et all, we can't 100% depend on its results so proceed with caution
-				@include($cachedir . '/data_' . $key . '.php');
-				if (!empty($expired) && isset($value))
-				{
-					@unlink($cachedir . '/data_' . $key . '.php');
-					unset($value);
-				}
-			}
-			break;
-	}
+function smf_get_eaccelerator($key, $ttl)
+{
+	// Again, eAccelerator.
+	if (function_exists('eaccelerator_get'))
+		return eaccelerator_get($key);
+	else
+		return null;
+}
 
-	if (isset($db_show_debug) && $db_show_debug === true)
+function smf_get_mmcache($key, $ttl)
+{
+	// The older, but ever-stable, Turck MMCache...
+	if (function_exists('mmcache_get'))
+		return mmcache_get($key);
+	else
+		return null;
+}
+
+function smf_get_apc($key, $ttl)
+{
+	// This is the free APC from PECL.
+	if (function_exists('apc_fetch'))
+		return apc_fetch($key . 'smf');
+	else
+		return null;
+}
+
+function smf_get_zend($key, $ttl)
+{
+	// Zend's pricey stuff.
+	if (function_exists('zend_shm_cache_fetch'))
+		return zend_shm_cache_fetch('SMF::' . $key, $ttl);
+	elseif (function_exists('output_cache_get'))
+		return output_cache_get($key, $ttl);
+	else
+		return null;
+}
+
+function smf_get_xcache($key, $ttl)
+{
+	if (function_exists('xcache_get') && ini_get('xcache.var_size') > 0)
+		return xcache_get($key);
+	else
+		return null;
+}
+
+function smf_get_filecache($key, $ttl)
+{
+	// Otherwise it's SMF data!
+	$key = strtr($key, ':/', '-_');
+
+	if (file_exists($cachedir . '/data_' . $key . '.php') && filesize($cachedir . '/data_' . $key . '.php') > 10)
 	{
-		$cache_hits[$cache_count]['t'] = array_sum(explode(' ', microtime())) - array_sum(explode(' ', $st));
-		$cache_hits[$cache_count]['s'] = isset($value) ? strlen($value) : 0;
+		// php will cache file_exists et all, we can't 100% depend on its results so proceed with caution
+		@include($cachedir . '/data_' . $key . '.php');
+		if (!empty($expired) && isset($value))
+		{
+			@unlink($cachedir . '/data_' . $key . '.php');
+			return null;
+		}
+		return $value;
 	}
 
-	if (function_exists('call_integration_hook'))
-		call_integration_hook('cache_get_data', array(&$key, &$ttl, &$value));
-
-	return empty($value) ? null : @unserialize($value);
+	return null;
 }
 
 /**
